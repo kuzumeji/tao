5a081eeb2a282aa039b95dd9aa8b44697354daad
nilcy <nilcy@mac.com>
2017-02-20 23:07:26 +0900

    FIX

--- src/main/java/com/github/kuzumeji/Library.java
+++ src/main/java/com/github/kuzumeji/Library.java
@@ -1,3 +1,9 @@
+/**
+ * Copyright (C) Kuzumeji Studios. All rights reserved.
+ * Licensed under the GNU AFFERO GENERAL PUBLIC LICENSE, Version 3 (the 'License').
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at https://www.gnu.org/licenses/agpl-3.0.txt
+ */
 package com.github.kuzumeji;
 /**
  * ライブラリ
@@ -8,6 +14,7 @@ public final class Library {
      * なにかのメソッド
      * @return true: false:
      */
+    @SuppressWarnings("static-method")
     public boolean someLibraryMethod() {
         return true;
     }
--- src/main/java/com/github/kuzumeji/package-info.java
+++ src/main/java/com/github/kuzumeji/package-info.java
@@ -1,5 +1,8 @@
 /**
- * 
+ * Copyright (C) Kuzumeji Studios. All rights reserved.
+ * Licensed under the GNU AFFERO GENERAL PUBLIC LICENSE, Version 3 (the 'License').
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at https://www.gnu.org/licenses/agpl-3.0.txt
  */
 /**
  * Kuzumeji
